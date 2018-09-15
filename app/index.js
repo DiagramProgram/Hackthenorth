8c578f70d920a88cf454d9710277c563e630f466
Wilson Wang <xerodragon72@gmail.com>
2018-09-15 12:57:10 -0400

    clean up and a little bit of code

--- app/index.js
+++ app/index.js
@@ -1,7 +1,9 @@
 import clock from "clock";
 import document from "document";
+import { display } from "display";
 import { preferences } from "user-settings";
 import * as util from "../common/utils";
+import { today } from "user-activity";
 
 console.log("App code started");
 
@@ -10,11 +12,17 @@ clock.granularity = "minutes";
 
 // Get a handle on the <text> element
 const timeDsp = document.getElementById("time");
+const stepDsp = document.getElementById("steps")
+
+display.autoOff = false;
+display.on = true;
 
 // Update the <text> element every tick with the current time
 clock.ontick = (evt) => {
-  let today = evt.date;
-  let hours = today.getHours();
+  let steps = today.local.steps || 0;
+  let todayDate = evt.date;
+  console.log(today.local.steps || 0);
+  let hours = todayDate.getHours();
   if (preferences.clockDisplay === "12h") {
     // 12h format
     hours = hours % 12 || 12;  
@@ -22,6 +30,7 @@ clock.ontick = (evt) => {
     // 24h format
     hours = util.zeroPad(hours);
   }
-  let mins = util.zeroPad(today.getMinutes());
+  let mins = util.zeroPad(todayDate.getMinutes());
   timeDsp.text = `${hours}:${mins}`;
+  stepDsp.text = (steps + "steps");
 }
